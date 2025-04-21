0872e5455aefce3864a13a59ac9097be21a4f41a
pushpak_261 <pushpakrajendra@gmail.com>
2025-04-21 19:51:22 +0530

    Your commit message here

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const bcrypt = require("bcrypt"); // ✅ Import bcrypt
+const bcrypt = require("bcryptjs"); // ✅ Import bcrypt
 const User = require('../models/user.js');
 
 const SALT_ROUNDS = 10; // You can adjust this as needed
